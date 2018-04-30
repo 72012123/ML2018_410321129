a5229d458771924c24a8ac978fdc66ed9ce25d74
72012123 <cv789789z@gmail.com>
2018-04-30 12:45:15 +0800

    Add files via upload

--- homework.py
+++ homework.py
@@ -3,35 +3,28 @@ import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 
-k1 = Image.open("key1.png")
-k2 = Image.open("key1.png")
-I = Image.open("I.png")
-E = Image.open("E.png")
-Eprime = Image.open("Eprime.png")
+k1 = imageio.imread('key1.png')
+k2 = imageio.imread('key2.png')
+I = imageio.imread('I.png')
+E = imageio.imread('E.png')
+Eprime = imageio.imread('Eprime.png')
 
 Epoch = 1
 w = np.random.rand(3)
-width = k1.width
-height = k1.height
-eta =  0.00001
 
-output = Image.new("L", (width, height), 0)
+eta =  0.00001
     
-while Epoch < 10:
-    for i in range(0, width):
-        for j in range(0, height):
-            a = w[0] * k1.getpixel((i, j)) + \
-                w[1] * k2.getpixel((i, j)) + \
-                w[2] * I.getpixel((i, j))
-            e = (E.getpixel((i, j)) - a)
-            w[0] = w[0] + eta * e * k1.getpixel((i, j))
-            w[1] = w[1] + eta * e * k2.getpixel((i, j))
-            w[2] = w[2] + eta * e * I.getpixel((i, j))
+while Epoch < 2:
+    for i in range(0, 300):
+        for j in range(0, 400):
+            a = w[0] * k1[i][j] + w[1] * k2[i][j] + w[2] * I[i][j]
+            e = (E[i][j] - a)
+            w[0] = w[0] + eta * e * k1[i][j]
+            w[1] = w[1] + eta * e * k2[i][j]
+            w[2] = w[2] + eta * e * I[i][j]
     Epoch += 1
 
-for i in range(0, width):
-    for j in range(0, height):
-        output.putpixel((i, j), int(round((Eprime.getpixel((i, j)) - w[0] * k1.getpixel((i, j)) - w[1] * k2.getpixel((i, j)))/w[2])))
-
-output.show()
-output.save("Eprime-decrypted.png")
+decrypted = (Eprime-(w[0]*k1)-(w[1]*k2))/w[2]
+print(w[0],w[1],w[2])
+plt.imshow(decrypted,plt.cm.gray)
+plt.show()
