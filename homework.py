080fe381c86a529ecc2664b38075c527abea7165
72012123 <cv789789z@gmail.com>
2018-04-30 09:34:50 +0800

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,23 +1,31 @@
 from PIL import Image
 import numpy as np
 
-Epoch = 1
 k1 = Image.open("key1.png")
 k2 = Image.open("key1.png")
 i = Image.open("I.png")
 E = Image.open("E.png")
 Eprime = Image.open("Eprime.png")
-w = [10, 10, 10]
+
+Epoch = 1
+w = np.array([[10, 10, 10]])
 width = k1.width
 height = k1.height
+eta = 1e-8
+error = [convergence()]
+
+def convergence(self)
+    
 
 while Epoch < 10 and :
     for i in range(0, width):
-        for j in range(0, height)
-            a[0] = w[0]*k1.getpixel((x, y))
-            a[1] = w[1]*k2.getpixel((x, y))
-            a[2] = w[2]*i.getpixel((x, y))
+        for j in range(0, height):
+            a[i][j] = w[0]*k1.getpixel((i, j)) + w[1]*k2.getpixel((i, j)) + w[2]*i.getpixel((i, j))
+            e[i][j] = E.getpixel((i, j)) - a[i][j]
+            w[0] += eta*e[i][j]*k1.getpixel((i, j))
+            w[1] += eta*e[i][j]*k2.getpixel((i, j))
+            w[2] += eta*e[i][j]*E.getpixel((i, j))
     Epoch += 1
-    
+
     
 
